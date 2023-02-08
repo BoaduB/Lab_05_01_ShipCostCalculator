5124f4900f65b5091405728f8edfd52df13a8dca
BoaduB <boansibk@mail.uc.edu>
2023-02-08 11:18:08 -0500

    Initial commit

---
+++ src/ShipCostCalculator.java
@@ -0,0 +1,26 @@
+public class ShipCostCalculator {
+    public static void main(String[] args) {
+
+        //Enter the price of your item
+        double itemPrice = 250;
+
+        if (itemPrice >= 100)
+        {
+            double shippingFee = 0;
+            double shippingPrice = itemPrice * shippingFee;
+            double totalPrice = itemPrice + shippingPrice;
+
+            System.out.println("The price for your shipping is $" +shippingPrice);
+            System.out.println("The total price of your item is $" +totalPrice);
+        }
+        else
+        {
+            double shippingFee = 0.02;
+            double shippingPrice = itemPrice * shippingFee;
+            double totalPrice = itemPrice + shippingPrice;
+
+            System.out.println("The price for your shipping is $" +shippingPrice);
+            System.out.println("The total price of your item is $" +totalPrice);
+        }
+    }
+}
